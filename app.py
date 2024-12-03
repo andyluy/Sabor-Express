7522fe7f65fe53a6577b96f0c93468140448240b
andyluy <andre.engenharia.uerj@gmail.com>
2024-12-03 20:44:43 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+print('''
+      丂闩⻏ㄖ尺 🝗〤尸尺🝗丂丂      
+      ''') 
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair')
+
+opcao_escolhida = input("Escolha uma opção: ")
+print (f'você escolheu a opção: {opcao_escolhida}')
